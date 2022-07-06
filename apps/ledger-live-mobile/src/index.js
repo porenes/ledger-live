92890b7d3cbb50208cce3b5ac5b334ad278852c0
Olivier Freyssinet <olivier.freyssinet@ledger.fr>
2022-07-06 18:30:36 +0200

    (squashed) Apply RN 0.68.2 diff
    https://react-native-community.github.io/upgrade-helper/?from=0.67.3&to=0.68.2
    
    Fix manifest (activity with intent filter should explicitly set android:exported)
    
    
    Keep gradle 4.2.2 because it seems to work fine (contrarily to 7.0.4)
    
    
    Remove react-native-gradle-plugin, compile fine without (& doesn't with it)
    
    
    Fix Java issues (+ cleanup unused imports)
    
    
    Keep Flipper 0.99.0, version 0.125.0 has some weird C++ shit crashing the app on start
    
    see https://github.com/facebook/flipper/issues/2948
    
    pnpm-lock changes
    
    Add comment regarding react-native-gesture-handler
    
    
    Upgrade lottie-react-native
    
    
    Properly rename AppDelegate.m to AppDelegate.mm in Xcode so config is updated
    
    
    Fix remaining "rndiffapp"
    
    
    Revert to previous AppDelegate
    
     The new AppDelegate.mm was useful only for the new RN arch (fabric & turbo modules) which we don't use
    
    Migrate from native RNGestureGandleEnabledRootView setup
    
    
    Fix scroll anim crashing on Portfolio because of animated border width
    
    This was not detected on prior versions of RN so it is probably related to the RN upgrade. For now the fix is to simply remove that border as it was invisible anyway (it was using the same color as the background). If we want to reimplement that later on, we will be able to do that by emulating a border at the bottom by using a simple view. 
    
    lint
    
    
    changeset
    
    
    Fix pnpm-lock.yaml after rebasing on develop
    
    Fix lint
    
    
    Fix android build workflow: upgrade java-version to 11 (needed for new RN)
    
    
    Fix podfile
    
    
    Fix iOS xcode build (add yargs in pnpmfile.cjs)
    
    
    Try installing android build tools manually
    
    
    empty commit
    
    Try fixing android build tools issue
    
    
    fix workflow syntax
    
    
    Revert to compileSdk,targetSdk 30 & build-tools 30.0.3 as build-tools 31 are broken
    
    
    pnpm-lock after rebase
    
    
    pnpm lockfile update

--- apps/ledger-live-mobile/src/index.js
+++ apps/ledger-live-mobile/src/index.js
@@ -3,7 +3,7 @@
 import "./polyfill";
 import "./live-common-setup";
 import "../e2e/e2e-bridge-setup";
-import "react-native-gesture-handler";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 import React, {
   Component,
   useCallback,
@@ -14,7 +14,6 @@ import React, {
 import { connect, useDispatch, useSelector } from "react-redux";
 import {
   StyleSheet,
-  View,
   Text,
   Linking,
   Appearance,
@@ -196,7 +195,7 @@ function App({ importDataString }: AppProps) {
   });
 
   return (
-    <View style={styles.root}>
+    <GestureHandlerRootView style={styles.root}>
       <SyncNewAccounts priority={5} />
       <ExperimentalHeader />
 
@@ -207,7 +206,7 @@ function App({ importDataString }: AppProps) {
       <FeatureToggle feature="ratings">
         <RatingsModal />
       </FeatureToggle>
-    </View>
+    </GestureHandlerRootView>
   );
 }
 
